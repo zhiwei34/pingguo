3a9ddeb5fe91cd78a10b1eb60d02e5909ac3aae9
Natalia Julian <zhiweiguanggao@gmail.com>
2026-01-18 11:52:40 -0300

    agrego m5u3

---
+++ M5/m5u3/models/bd.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql2');
+var util = require('util');
+
+var pool = mysql.createPool({
+    connectionLimit: 10,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB_NAME
+});
+
+pool.query = util.promisify(pool.query);
+
+const promisePool = pool.promise();
+module.exports = pool;
